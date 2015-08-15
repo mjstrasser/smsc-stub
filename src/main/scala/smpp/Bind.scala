9738ac91118f8a5c36965240bdeb3bc40e19a32b
Michael Strasser <michael@michaelstrasser.com>
2015-08-15 10:33:48 +1000

    More hacking about.

--- src/main/scala/smpp/Bind.scala
+++ src/main/scala/smpp/Bind.scala
@@ -2,8 +2,8 @@ package smpp
 
 case class BindBody(systemId: String, password: String, systemType: String,
                     interfaceVersion: Byte, addrTon: Byte, addrNpi: Byte, addressRange: String)
-abstract class Bind(header: Header, bindBody: BindBody)
-abstract class BindResp(header: Header, systemId: String)
+abstract class Bind(header: Header, bindBody: BindBody) extends Pdu
+abstract class BindResp(header: Header, systemId: String) extends Pdu
 
 case class BindTransmitter(header: Header, bindBody: BindBody) extends Bind(header, bindBody)
 case class BindTransmitterResp(header: Header, systemId: String) extends BindResp(header, systemId)
--- src/main/scala/smsc/SmscHandler.scala
+++ src/main/scala/smsc/SmscHandler.scala
@@ -3,16 +3,22 @@ package smsc
 import akka.actor.{Actor, ActorLogging}
 import akka.io.Tcp
 import akka.util.ByteString
+import smpp.{BindTransmitter, Pdu}
 
 class SmscHandler extends Actor with ActorLogging {
 
   import Tcp._
 
-  def handlePdu(data: ByteString) = ???
+  def responseTo(data: ByteString): ByteString = {
+    Pdu.fromRequest(data) match {
+      case BindTransmitter => ???
+      case _ => ???
+    }
+  }
 
   def receive = {
     case Received(data) =>
-      handlePdu(data)
+      sender ! Write(responseTo(data))
     case PeerClosed =>
       log.info("Disconnected")
       context stop self
